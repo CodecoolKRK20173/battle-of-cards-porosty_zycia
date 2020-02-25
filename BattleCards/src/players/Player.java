3121d47ca1b3acd58453c785ad59aec329bae721
StaNC2000 <stanislaw.chynek@gmail.com>
2020-02-25 13:45:46 +0100

    players

---
+++ BattleCards/src/players/Player.java
@@ -0,0 +1,13 @@
+package players;
+
+public abstract class Player {
+    private String name;
+
+    public Player(String name) {
+        this.name = name;
+    }
+
+    public void makeHand() {
+
+    }
+}
